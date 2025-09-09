0f0f9186f4fbe52c927225f66294a2093686abd4
ordkiki <onjaniaina235497@gmail.com>
2025-09-09 11:31:27 +0300

    style de l affichage de l export

--- src/RanjaKen.Infrastructure/Persistences/Services/ExcelService.cs
+++ src/RanjaKen.Infrastructure/Persistences/Services/ExcelService.cs
@@ -1,4 +1,5 @@
 ﻿using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 using Ranjaken.Domain.Entities;
 using Ranjaken.Domain.Interfaces.Services;
 
@@ -13,11 +14,12 @@ namespace RanjaKen.Infrastructure.Persistences.Services
             IXLWorksheet worksheet = workbook.Worksheets.Add("Players");
 
             // Header
-            string[] headers = { "FirstName", "LastName", "Age", "Idole","Pseudo", "Idole", "Size", "Created At" };
+            string[] headers = { "FirstName", "LastName", "Age", "Idole","Pseudo", "Size", "Created At" };
             for (int i = 0; i < headers.Length; i++)
             {
                 worksheet.Cell(1, i + 1).Value = headers[i];
                 worksheet.Cell(1, i + 1).Style.Font.Bold = true;
+                worksheet.Cell(1, i + 1).Style.Fill.BackgroundColor = XLColor.LightBlue;
             }
             int row = 2;
             foreach (Player p in players)
@@ -25,10 +27,11 @@ namespace RanjaKen.Infrastructure.Persistences.Services
                 worksheet.Cell(row, 1).Value = p.LastName;
                 worksheet.Cell(row, 2).Value = p.FirstName;
                 worksheet.Cell(row, 3).Value = p.Age;
-                worksheet.Cell(row, 3).Value = p.Idole;
-                worksheet.Cell(row, 4).Value = p.Pseudo;
-                worksheet.Cell(row, 5).Value = p.Idole;
-                worksheet.Cell(row, 6).Value = p.Size;    
+                worksheet.Cell(row, 4).Value = p.Idole;
+                worksheet.Cell(row, 5).Value = p.Pseudo;
+                worksheet.Cell(row, 6).Value = p.Size;
+                worksheet.Cell(row, 7).Value = p.CreatedAt;
+                worksheet.Cell(row, 7).Style.DateFormat.Format = "yyyy-MM-dd HH:mm:ss";
                 row++;
             }
             worksheet.Columns().AdjustToContents();
